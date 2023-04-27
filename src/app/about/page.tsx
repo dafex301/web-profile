24d284b02fe5d3be7c174055df27a54755ac82de
Fahrel <fahrel@students.undip.ac.id>
2023-04-28 00:18:38 +0700

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Fahrel's About",
+  description: "Fahrel Gibran Alghany Web Profile Created using Next.js",
+};
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,117 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const ICON_SIZE = 25;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      {/* Header */}
+      <header className="bg-gray-700">
+        <div className="flex justify-between mx-64 pt-8">
+          <Link href={"/"} className="text-white font-bold text-2xl">
+            DAFEX
+          </Link>
+          <div className="flex gap-4 font-semibold">
+            <Link
+              href={"/"}
+              className="text-gray-100 flex flex-col items-center"
+            >
+              <div>Home</div>
+              <div className="w-2 h-2 rounded-full bg-yellow-500"></div>
+            </Link>
+            <Link
+              href={""}
+              className="text-gray-500 opacity-60 hover:opacity-100 hover:text-gray-200"
+            >
+              About
+            </Link>
+          </div>
+        </div>
+      </header>
+      {/* End of Header */}
+
+      {/* Hero */}
+      <main className="bg-gray-700 text-white">
+        <div className="mx-64">
+          <div className="flex gap-3">
+            {/* Left Layout */}
+            <div className="flex flex-col justify-center">
+              <h1 className="text-5xl font-bold">
+                Fahrel
+                <br />
+                Gibran
+                <br />
+                Alghany.
+              </h1>
+              <div className="w-12 h-2 mt-3 bg-yellow-500"></div>
+              <div className="flex mt-5 gap-2">
+                <Link
+                  target="_blank"
+                  href={"https://www.linkedin.com/in/fahrelga/"}
+                >
+                  <Image
+                    src={"/assets/icons/linkedin.svg"}
+                    alt={"Linkedin"}
+                    width={ICON_SIZE}
+                    height={ICON_SIZE}
+                  ></Image>
+                </Link>
+                <Link target="_blank" href={"https://github.com/dafex301"}>
+                  <Image
+                    src={"/assets/icons/github.svg"}
+                    alt={"GitHub"}
+                    width={ICON_SIZE}
+                    height={ICON_SIZE}
+                  ></Image>
+                </Link>
+              </div>
+            </div>
+            {/* End of Left Layout */}
+            <div>
+              <Image
+                alt="Cover"
+                src={"/cover.png"}
+                width={500}
+                height={500}
+              ></Image>
+            </div>
+            <div className="flex flex-col justify-center">
+              <h3 className="text-sm text-gray-400">Introduction</h3>
+              <h2 className="text-2xl mb-5 mt-2 font-semibold">
+                CEO of Racism
+              </h2>
+              <p className="text-xs text-gray-400 mb-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                ipsum distinctio, atque nulla harum blanditiis ea quae accusamus
+                libero ratione.
+              </p>
+              <Link
+                href={"https://wa.me/+6281270360350"}
+                target="_blank"
+                className="text-sm font-semibold inline hover:cursor-pointer text-yellow-500 hover:underline"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+      {/* End of Hero */}
+
+      {/* Content */}
+      <div className="bg-gray-800 text-white">
+        <div className="mx-64 py-16">
+          <center>
+            <h2 className="text-2xl">Specialities</h2>
+            <p className="text-sm text-gray-300">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum at
+              quod consequatur, placeat nemo obcaecati debitis optio natus
+              blanditiis et?
+            </p>
+          </center>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {/* End of Content */}
+    </>
+  );
 }
